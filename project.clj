fa1423ca549e15ce2443cfd56876d86c6009ff35
shixiongfei <xiongfei.shi@icloud.com>
2023-12-13 00:07:07 +0800

    Add jedis connection pool

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
-(defproject redque-clojure "0.1.0"
+(defproject redque "0.1.0"
   :description "A reliable message queue base on Redis"
   :url "https://github.com/shixiongfei/redque-clojure"
   :license {:name "Apache 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :repl-options {:init-ns redque-clojure.core})
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [redis.clients/jedis "5.0.1"]
+                 [org.clojure/data.json "2.4.0"]
+                 [nano-id "1.0.0"]])
---
+++ src/redque/redis.clj
@@ -0,0 +1,52 @@
+(ns redque.redis
+  (:require [clojure.string :as string])
+  (:import [redis.clients.jedis JedisPool JedisPoolConfig]))
+
+(defn- parse-redis-url [url]
+  (let [uri         (java.net.URI. url)
+        port        (.getPort uri)
+        user-info   (or (.getUserInfo uri) "")
+        user-passwd (string/split user-info #":")
+        path        (.getPath uri)]
+    {:scheme   (.getScheme uri)
+     :host     (.getHost uri)
+     :port     (if (> port 0) port 6379)
+     :user     (get user-passwd 0 "")
+     :password (get user-passwd 1 "")
+     :database (if (= path "") 0
+                   (Integer. (subs path 1)))}))
+
+(defn create-redis-pool [url & {:keys [max-total max-idle min-idle]
+                                :or   {max-total 3 max-idle  2 min-idle  1}}]
+  (let [redis-conf (parse-redis-url url)
+        ssl        (= (:scheme redis-conf) "rediss")
+        pool-conf  (doto (JedisPoolConfig.)
+                     (.setMaxTotal max-total)
+                     (.setMaxIdle  max-idle)
+                     (.setMinIdle  min-idle))]
+    (if (not= (:user redis-conf) "")
+      (JedisPool. pool-conf
+                  (:host redis-conf)
+                  (:port redis-conf)
+                  3000
+                  (:user redis-conf)
+                  (:password redis-conf)
+                  (:database redis-conf)
+                  ssl)
+      (JedisPool. pool-conf
+                  (:host redis-conf)
+                  (:port redis-conf)
+                  3000
+                  (:password redis-conf)
+                  (:database redis-conf)
+                  ssl))))
+
+(defn connect [jedis-pool] (.getResource jedis-pool))
+
+(defn disconnect [jedis] (.close jedis))
+
+(defmacro with [jedis-pool [jedis] & body]
+  `(let [~jedis (connect ~jedis-pool)]
+     (try
+       ~@body
+       (finally (disconnect ~jedis)))))
