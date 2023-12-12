04cbd3d5922a8c5b360b3bedf35338089819fe02
shixiongfei <xiongfei.shi@icloud.com>
2023-12-13 00:25:15 +0800

    Completed simple queue

---
+++ src/redque/simple_queue.clj
@@ -0,0 +1,21 @@
+(ns redque.simple-queue
+  (:require [clojure.data.json :as json]
+            [redque.redis :as redis]))
+
+(defrecord SimpleQueue [redis-pool])
+
+(defn make-simple-queue [url]
+  (SimpleQueue. (redis/create-redis-pool url)))
+
+(defn produce [simple-queue name payload]
+  (redis/with (:redis-pool simple-queue) [jedis]
+    (.lpush jedis name (into-array String [(json/write-str payload)]))))
+
+(defn consume [simple-queue name & {:keys [timeout]}]
+  (if (nil? timeout)
+    (when-some [payload (redis/with (:redis-pool simple-queue) [jedis]
+                          (.rpop jedis name))]
+      (json/read-str payload :key-fn keyword))
+    (when-some [payload (redis/with (:redis-pool simple-queue) [jedis]
+                          (.brpop jedis (double timeout) name))]
+      (json/read-str (.getValue payload) :key-fn keyword))))
