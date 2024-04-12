0c6de9fccf553698b431e42d6fccc28b025e915b
Xiongfei Shi <jenson.shixf@gmail.com>
2024-04-12 17:11:26 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
 (defproject redque "0.1.0"
   :description "A reliable message queue base on Redis"
   :url "https://github.com/shixiongfei/redque-clojure"
-  :license {:name "Apache 2.0"
+  :license {:name "Apache-2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.txt"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [redis.clients/jedis "5.0.1"]
